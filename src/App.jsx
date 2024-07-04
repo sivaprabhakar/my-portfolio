52729b97a32876ba5074a4c01f4cb46df066faa9
sivaprabhakar <sivaprabhakaran.94@gmail.com>
2024-07-04 12:24:45 +0530

    extra width removed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@
 //   return (
 //     <>
     
-//     <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:tex-cyan-900'>
+//     <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
 //     <div className='fixed top-0 -z-10 h-full w-full'>
 //       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
 //     </div>
@@ -48,20 +48,18 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-      <div className=' text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
+      <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
         <div className='fixed top-0 -z-10 h-full w-full'>
           <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
         </div>
         <Header />
         <div className='container mx-auto px-8'>
-          
           <Hero />
           <About />
           <Skills />
           <Projects />
           <Contact />
           <Footer />
-          
         </div>
       </div>
     </>
